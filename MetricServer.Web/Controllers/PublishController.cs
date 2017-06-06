612f343f88bdbbb5983a8d5a4c02169febc8a8d9
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2017-06-06 20:53:16 +0200

    Initial commit

---
+++ MetricServer.Web/Controllers/PublishController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricServer.Web.Controllers
+{
+    public class PublishController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(string project, string branch, IFormFile file)
+        {
+            return View();
+        }
+    }
+}
